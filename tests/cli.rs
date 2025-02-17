6f0081e4992818c8e0aead6ae7dd3e86dc2f762e
Sam Paxton <spaxton3397@gmail.com>
2025-02-17 11:32:28 -0500

    fix integration tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,7 +5,7 @@ use std::process::Command; // Run programs
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("grrs")?;
+    let mut cmd = Command::cargo_bin("nconf")?;
 
     cmd.arg("foobar").arg("test/file/doesnt/exist");
     cmd.assert()
@@ -17,7 +17,7 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 fn pattern_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     let file = assert_fs::NamedTempFile::new("sample.txt")?;
-    let mut cmd = Command::cargo_bin("grrs")?;
+    let mut cmd = Command::cargo_bin("nconf")?;
     cmd.arg("").arg(file.path());
     cmd.assert()
         .failure()
@@ -31,7 +31,7 @@ fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
     let file = assert_fs::NamedTempFile::new("sample.txt")?;
     file.write_str("A test\nActual content\nMore content\nAnother test")?;
 
-    let mut cmd = Command::cargo_bin("grrs")?;
+    let mut cmd = Command::cargo_bin("nconf")?;
     cmd.arg("test").arg(file.path());
     cmd.assert()
         .success()
